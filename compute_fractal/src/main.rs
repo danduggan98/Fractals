c4253455558117f88feb20450136f83b2aaa2770
danduggan98 <dadu3890@colorado.edu>
2020-08-17 22:25:05 -0600

    Iterate over the canvas pixels

--- compute_fractal/src/main.rs
+++ compute_fractal/src/main.rs
@@ -1,7 +1,13 @@
 fn main() {
-    compute_fractal(256, 256);
+    compute_fractal(2/*56*/, 2/*56*/);
 }
 
 fn compute_fractal(w: i32, h: i32) {
     println!("Computing fractal with width {} and height {}", w, h);
+
+    for pixel_x in 0..w {
+        for pixel_y in 0..h {
+            println!("Computing pixel at ({}, {})", pixel_x, pixel_y);
+        }
+    }
 }
