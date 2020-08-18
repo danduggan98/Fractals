ba2ce01063c36d1a6bf0a8304a8c1d4a8a047244
danduggan98 <dadu3890@colorado.edu>
2020-08-17 21:50:31 -0600

    Function takes in canvas width and height

--- compute_fractal/src/main.rs
+++ compute_fractal/src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    compute_fractal(256, 256);
+}
+
+fn compute_fractal(w: i32, h: i32) {
+    println!("Computing fractal with width {} and height {}", w, h);
 }
