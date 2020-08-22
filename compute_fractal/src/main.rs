4f73db0ad2c72d5ac99fe4289dc7bec05d1e4a2f
danduggan98 <dadu3890@colorado.edu>
2020-08-21 20:33:18 -0600

    Formula now uses current x and y instead of zero

--- compute_fractal/src/main.rs
+++ compute_fractal/src/main.rs
@@ -1,10 +1,11 @@
 // Performance improvements
-// - Use ndarray's instead of vectors for faster traversal
+// - Use ndarrays instead of vectors for faster traversal
 // - Try smaller data types for certain values
+// - Add benchmarking using the time crate
 
 fn main() {
     const MAX_ITERATIONS: u32 = 100;
-    let result = compute_fractal(2000, 1500, 0.0, 0.0, MAX_ITERATIONS); //Test value
+    let result = compute_fractal(20, 15, 0.0, 0.0, MAX_ITERATIONS); //Test value
 
     println!("There were {} points on the mandelbrot", check_fractal(result, MAX_ITERATIONS));
 }
@@ -15,7 +16,7 @@ fn compute_fractal(width: usize, height: usize, x_0: f32, y_0: f32, max_iteratio
     //Store the result in 2-dimensional vector
     let mut results_array = vec![vec![0; width]; height];
 
-    //Convert the pixels into real coordinates
+    //Convert the pixels into real coordinates in the Mandelbrot's range
     const X_MIN: f32 = -2.0;
     const X_MAX: f32 = 1.0;
     const Y_MIN: f32 = -1.0;
@@ -37,32 +38,36 @@ fn compute_fractal(width: usize, height: usize, x_0: f32, y_0: f32, max_iteratio
     let mut pixel_x = X_MIN as f32;
     let mut pixel_y = Y_MIN as f32;
     let mut array_row: usize = 0;
+    let mut array_col: usize = 0;
 
     while pixel_x < X_MAX {
         while pixel_y < Y_MAX {
-            y = 0.0;
+            x = pixel_x;
+            y = pixel_y;
             x_squared = 0.0;
             y_squared = 0.0;
             iterations = 0;
 
             while x_squared + y_squared <= 4.0 && iterations < max_iterations {
-                x = x_squared - y_squared + x_0;
                 y = 2.0 * x * y + y_0;
+                x = x_squared - y_squared + x_0;
                 x_squared = x * x;
                 y_squared = y * y;
                 iterations += 1;
             }
-            pixel_y += pixel_height;
-
             //Convert the number of iterations to a color value, then store it
             //Need a const to multiply the iteration num by to map it between white and black
             //..convert..
-            results_array[array_row].push(iterations);
-            //println!("({},{}) = {}", pixel_x, pixel_y, iterations);
+            results_array[array_row][array_col] = iterations;
 
+            println!("({},{}) = {}", pixel_x, pixel_y, iterations);
+            pixel_y += pixel_height;
+            array_col += 1;
         }
+        pixel_y = Y_MIN;
         pixel_x += pixel_width;
         array_row += 1;
+        array_col = 0;
     }
     return results_array;
 }
