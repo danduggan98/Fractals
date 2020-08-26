555839987b95eaa1ca81cca772717da02401e5d9
danduggan98 <dadu3890@colorado.edu>
2020-08-26 13:20:35 -0600

    Removed unused css properties

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,15 +1,14 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Fractals.io - Mandelbrot</title>
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
+      <main>
+        <h1>
           The Mandelbrot Set
         </h1>
         <canvas id='fractalCanvas' width={500} height={500}></canvas>
