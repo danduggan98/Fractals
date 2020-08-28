c6933286c010a9113ce6f79c5d9ef088287be5d6
danduggan98 <dadu3890@colorado.edu>
2020-08-28 06:56:42 -0600

    Load the mandelbrot script direcly in JSX

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -5,13 +5,17 @@ export default function Home() {
     <div>
       <Head>
         <title>Fractals.io - Mandelbrot</title>
+        <script
+            type='text/javascript'
+            src='./../scripts/mandelbrot.js'>
+        </script>
       </Head>
 
       <main>
         <h1>
           The Mandelbrot Set
         </h1>
-        <canvas id='fractalCanvas' width={500} height={500}></canvas>
+        <canvas id='fractalCanvas' width={400} height={400}></canvas>
       </main>
     </div>
   )
