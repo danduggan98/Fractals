efb41d2fd73a6c387dde8315505ca8abfed525af
danduggan98 <dadu3890@colorado.edu>
2020-08-29 12:00:03 -0600

    Successfully passed pixel data to ImageData obj

--- experiments/init.js
+++ experiments/init.js
@@ -4,21 +4,17 @@ import { memory } from './pkg/experiments_bg';
 (function loadMandelbrot() {
     const fractalCanvas = document.getElementById('fractalCanvas');
     const ctx = fractalCanvas.getContext('2d');
+
     const width = fractalCanvas.width;
     const height = fractalCanvas.height;
 
     const pixels = wasm.mandelbrot(width, height, -2.0, 0.0, 25);
     console.log('Initial data:', pixels);
 
-    const fractal = new Uint8ClampedArray(
-        memory.buffer,
-        pixels,
-        (4 * width * height)
-    )
-
-    const fractalImage = new ImageData(fractal, width, height);
+    const fractalArray = new Uint8ClampedArray(pixels);
+    const fractalImage = new ImageData(fractalArray, width, height);
     console.log('Initial image data:', fractalImage.data);
-    
+
     ctx.putImageData(fractalImage, 0, 0);
     console.log('Final image data:', ctx.getImageData(0,0,width,height));
 })();
