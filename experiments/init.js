9e18bfc41f7f14effbc1d47fb1161e0495b9e75e
danduggan98 <dadu3890@colorado.edu>
2020-08-29 20:23:54 -0600

    Added explanations for each step

--- experiments/init.js
+++ experiments/init.js
@@ -1,20 +1,22 @@
+//DATA IS THERE, BUT ITS NOT VISIBLE???
+
 import * as wasm from './pkg';
-import { memory } from './pkg/experiments_bg';
 
 (function loadMandelbrot() {
     const fractalCanvas = document.getElementById('fractalCanvas');
-    const ctx = fractalCanvas.getContext('2d');
-
     const width = fractalCanvas.width;
     const height = fractalCanvas.height;
 
+    //Create an image from our mandelbrot results
     const pixels = wasm.mandelbrot(width, height, -2.0, 0.0, 25);
-    console.log('Initial data:', pixels);
-
     const fractalArray = new Uint8ClampedArray(pixels);
     const fractalImage = new ImageData(fractalArray, width, height);
-    console.log('Initial image data:', fractalImage.data);
 
+    //Store the image in our canvas
+    const ctx = fractalCanvas.getContext('2d');
     ctx.putImageData(fractalImage, 0, 0);
+
+    console.log('Initial image data:', fractalImage.data);
+    console.log('Initial data:', pixels);
     console.log('Final image data:', ctx.getImageData(0,0,width,height));
 })();
