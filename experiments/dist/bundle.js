9fb0778bb00dcaada0523451fd55f98f154dcd7c
danduggan98 <dadu3890@colorado.edu>
2020-09-04 19:48:03 -0600

    New build/WASM with optimizations

--- experiments/dist/bundle.js
+++ experiments/dist/bundle.js
@@ -155,7 +155,7 @@
 /******/ 				promises.push(installedWasmModuleData);
 /******/ 			else {
 /******/ 				var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 				var req = fetch(__webpack_require__.p + "" + {"./pkg/experiments_bg.wasm":"4703f25e133904a45aa7"}[wasmModuleId] + ".module.wasm");
+/******/ 				var req = fetch(__webpack_require__.p + "" + {"./pkg/experiments_bg.wasm":"83de21b561d35938b696"}[wasmModuleId] + ".module.wasm");
 /******/ 				var promise;
 /******/ 				if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 					promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
