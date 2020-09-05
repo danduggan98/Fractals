e8ae6c267cbc73aad06d414178836a6d00fb1adf
danduggan98 <dadu3890@colorado.edu>
2020-09-04 18:40:28 -0600

    More iterations since we can afford it

--- experiments/init.js
+++ experiments/init.js
@@ -15,7 +15,7 @@ const Y_MAX = 1.2;
 
     //Create an image from our mandelbrot results
     //const pixels = wasm.mandelbrot(width, height, -1.0, -0.5, 0.0, 0.5, 64);
-    const pixels = wasm.mandelbrot(width, height, X_MIN, X_MAX, Y_MIN, Y_MAX, 64);
+    const pixels = wasm.mandelbrot(width, height, X_MIN, X_MAX, Y_MIN, Y_MAX, 128);
     const fractalArray = new Uint8ClampedArray(pixels);
     const fractalImage = new ImageData(fractalArray, width, height);
 
