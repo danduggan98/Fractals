d5da3b97ed36ff74200c4923f38c22a484c1c991
danduggan98 <dadu3890@colorado.edu>
2020-09-07 08:15:52 -0600

    Removed default css files

--- fractals/pages/index.js
+++ fractals/pages/index.js
@@ -1,10 +1,12 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
 import Mandelbrot from '../components/mandelbrot';
 
 export default function Home() {
     return (
         <div>
+            <Head>
+                Fractals.io - Mandelbrot
+            </Head>
             <div>Welcome to Fractals.io!</div>
             <Mandelbrot />
         </div>
