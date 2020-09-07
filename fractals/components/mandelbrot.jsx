1d3444c8b7f2163b0f1eeee4b12be034d0c5dba0
danduggan98 <dadu3890@colorado.edu>
2020-09-06 21:40:58 -0600

    Load the image using a single function

--- fractals/components/mandelbrot.jsx
+++ fractals/components/mandelbrot.jsx
@@ -6,19 +6,16 @@ const Y_MIN = -1.2;
 const Y_MAX = 1.2;
 const max_iterations = 128;
 
-const mandelbrotData = async (width, height) => {
-    const wasmPackage = await import('../fractal_utils/pkg');
-    return wasmPackage.mandelbrot(width, height, X_MIN, X_MAX, Y_MIN, Y_MAX, max_iterations);
-};
+const populateCanvas = async (x_0, x_1, y_0, y_1, iterations) => {
+    const wasm = await import('../fractal_utils/pkg');
 
-const populateCanvas = async () => {
     //Find the canvas, get its dimensions
     const fractalCanvas = document.getElementById('mandelbrotCanvas');
-    const width = fractalCanvas.width;
+    const width  = fractalCanvas.width;
     const height = fractalCanvas.height;
 
     //Run the function and save the result as an image
-    const data = await mandelbrotData(width, height);
+    const data = wasm.mandelbrot(width, height, x_0, x_1, y_0, y_1, iterations);
     const fractalArray = new Uint8ClampedArray(data);
     const fractalImage = new ImageData(fractalArray, width, height);
 
@@ -29,14 +26,16 @@ const populateCanvas = async () => {
 
 export default class Mandelbrot extends Component {
     async componentDidMount() {
-        await populateCanvas();
+        console.time('Mandelbrot loaded in');
+        await populateCanvas(X_MIN, X_MAX, Y_MIN, Y_MAX, max_iterations);
+        console.timeEnd('Mandelbrot loaded in');
     }
 
     render() {
         return (
             <div>
                 <div>mandelbrot!</div>
-                <canvas id='mandelbrotCanvas' width={800} height={800}></canvas>
+                <canvas id='mandelbrotCanvas' width={1500} height={1500}></canvas>
             </div>
         )
     }
