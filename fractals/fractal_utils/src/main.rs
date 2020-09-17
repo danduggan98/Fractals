4f8ed98f72fc9d7628ae976b99ecd2b0e8e7d242
danduggan98 <dadu3890@colorado.edu>
2020-09-17 11:23:36 -0600

    Main function now measures performance with default colors/iterations

--- fractals/fractal_utils/src/main.rs
+++ fractals/fractal_utils/src/main.rs
@@ -2,20 +2,16 @@ mod lib;
 
 //Test the mandelbrot method
 fn main() {
-    const MAX_ITERATIONS: u8 = 100;
-    let arr = vec![0,0,0, 255,0,0];
-    let result = lib::mandelbrot(1000, 1000, -1.0, -0.5, 0.0, 0.5, 128, arr); //Test value
-    println!("There were {} points on the mandelbrot", check_fractal(result, MAX_ITERATIONS));
-}
+    const MAX_ITERATIONS: u32 = 100;
+    let arr = vec![0,0,0, 0,0,255, 255,255,255, 255,255,0, 255,255,255];
 
-fn check_fractal(data: Vec<u8>, max: u8) -> u32 {
-    let mut members: u32 = 0;
+    //Measure performance
+    use std::time::Instant;
+    let now = Instant::now();
+    let _result = lib::mandelbrot(1000, 1000, -1.0, -0.5, 0.0, 0.5, MAX_ITERATIONS, arr);
+    let elapsed = now.elapsed();
 
-    //Count the number of points that fall within the set
-    for pixel in data.iter() {
-        if pixel < &max {
-            members += 1;
-        }
-    }
-    return members;
+    println!("Mandelbrot function completed successfully!");
+    println!("- Iterations: {}", MAX_ITERATIONS);
+    println!("- Time elapsed: {} ms", elapsed.as_millis());
 }
