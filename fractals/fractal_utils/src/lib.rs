a6c46656049810fc16ef04d96d8a8397b7e71a40
danduggan98 <dadu3890@colorado.edu>
2020-09-19 12:55:26 -0600

    Slightly more streamlined code/comments
    Also calculates color palette with f64 so more colors are possible

--- fractals/fractal_utils/src/lib.rs
+++ fractals/fractal_utils/src/lib.rs
@@ -12,35 +12,38 @@ pub fn mandelbrot(width: u32, height: u32, x_0: f64, x_1: f64, y_0: f64, y_1: f6
 
     //////// COLOR SCHEME ////////
     
-    //Create the color palette based on a list of colors
+    //Convert the list of RGB values to a Color vector
     let mut color_list: Vec<Color> = Vec::new();
     for c in (0..colors.len()).step_by(3) {
         color_list.push(Color { r: colors[c], g: colors[c+1], b: colors[c+2] });
     }
 
+    //Create a color palette with smooth gradients between each item in our Color vector
+    // - E.g. given the colors red, yellow, and purple, our palette would have two consecutive gradients:
+    //   one between red and yellow followed by one between yellow and purple
+
     let mut color_palette: Vec<Color> = Vec::new();
-    let num_colors = color_list.len();
-    let palette_size: u32 = max_iterations * 10;
-    let section_width = palette_size / (num_colors - 1) as u32;
+    let palette_size = max_iterations * 10;
+    let num_gradients = color_list.len() - 1;
+    let gradient_width = palette_size / num_gradients as u32; //Number of colors in each gradient
 
     let mut start_color: &Color;
-    let mut end_color: &Color = &color_list[0]; //Initialized here in case num_colors = 1
-    let (mut delta_r, mut delta_g, mut delta_b): (f32, f32, f32);
-    let (mut r_val, mut g_val, mut b_val): (f32, f32, f32);
+    let mut end_color: &Color = &color_list[0];
+    let (mut delta_r, mut delta_g, mut delta_b): (f64, f64, f64);
+    let (mut r_val, mut g_val, mut b_val): (f64, f64, f64);
 
-    for _color_idx in 0..(num_colors - 1) { //Loop through all colors to make gradients between each
-        start_color = &color_list[_color_idx];
-        end_color   = &color_list[_color_idx + 1];
+    for color_idx in 0..num_gradients {
+        start_color = &color_list[color_idx];
+        end_color   = &color_list[color_idx + 1];
 
-        delta_r = (end_color.r as f32 - start_color.r as f32) / section_width as f32;
-        delta_g = (end_color.g as f32 - start_color.g as f32) / section_width as f32;
-        delta_b = (end_color.b as f32 - start_color.b as f32) / section_width as f32;
+        delta_r = (end_color.r as f64 - start_color.r as f64) / gradient_width as f64;
+        delta_g = (end_color.g as f64 - start_color.g as f64) / gradient_width as f64;
+        delta_b = (end_color.b as f64 - start_color.b as f64) / gradient_width as f64;
+        r_val = start_color.r as f64;
+        g_val = start_color.g as f64;
+        b_val = start_color.b as f64;
 
-        r_val = start_color.r as f32;
-        g_val = start_color.g as f32;
-        b_val = start_color.b as f32;
-
-        for _palette_idx in 0..section_width {
+        for _palette_idx in 0..gradient_width {
             r_val += delta_r;
             g_val += delta_g;
             b_val += delta_b;
@@ -55,26 +58,23 @@ pub fn mandelbrot(width: u32, height: u32, x_0: f64, x_1: f64, y_0: f64, y_1: f6
 
     //////// GENERATING THE SET ////////
     
-    //Store the result in 2-dimensional vector
-    let size: usize = (width * height * 4) as usize; //4x to use RGBA values
-    let mut results_array = vec![0; size];
+    //Array to store our RGBA values
+    let output_size = (width * height * 4) as usize;
+    let mut results_array = vec![0; output_size];
 
-    //Convert the pixels into real coordinates in the Mandelbrot's range
-    let pixel_width  = (x_1 - x_0) / (width as f64);
-    let pixel_height = (y_1 - y_0) / (height as f64);
-    println!("Using pixel width {} and pixel height {}", pixel_width, pixel_height);
-
-    //Formula values
     let (mut x, mut y, mut x_squared, mut y_squared, mut iterations): (f64, f64, f64, f64, u32);
     let mut pixel_x = x_0;
     let mut pixel_y = y_0;
-    let mut pixel_idx: usize = 0; //Keeps track of our location in the array
+    let mut pixel_idx: usize = 0; //Tracks our location in the array
     let mut pixel_color: &Color;
+    let pixel_width  = (x_1 - x_0) / (width as f64);
+    let pixel_height = (y_1 - y_0) / (height as f64);
 
     //Log constants used to determine shading
+    //Credit to Christian Stigen Larsen and Linas Vepstas for the smooth coloring tricks
     let log_base = 1.0 / (2.0 as f64).log10();
     let log_half_base = (0.5 as f64).log10() * log_base;
-    let spread_multiplier: f64 = ((palette_size - 1) / max_iterations) as f64;
+    let spread_multiplier = ((palette_size - 1) / max_iterations) as f64;
 
     //Calculate the result for each pixel
     for _array_y in 0..height {
@@ -95,11 +95,9 @@ pub fn mandelbrot(width: u32, height: u32, x_0: f64, x_1: f64, y_0: f64, y_1: f6
 
             //Store the number of iterations as an RGBA color value using logarithmic shading
             if iterations < max_iterations {
-                let z: f64 = x_squared + y_squared;
+                let z = x_squared + y_squared;
                 let mu = ((iterations + 5) as f64) - log_half_base - (z.log10().log10() * log_base);
                 let n = (mu * spread_multiplier).floor() as usize;
-
-                //println!("iterations: {}, spread mult: {}, mu: {}, n:{}", iterations, spread_multiplier, mu, n);
 
                 pixel_color = &color_palette[n];
                 results_array[pixel_idx]     = pixel_color.r as u8;
@@ -111,7 +109,7 @@ pub fn mandelbrot(width: u32, height: u32, x_0: f64, x_1: f64, y_0: f64, y_1: f6
                 results_array[pixel_idx + 1] = 0;
                 results_array[pixel_idx + 2] = 0;
             }
-            results_array[pixel_idx + 3] = 255;
+            results_array[pixel_idx + 3] = 255; //All values are fully opaque
             pixel_idx += 4;
             pixel_x += pixel_width;
         }
